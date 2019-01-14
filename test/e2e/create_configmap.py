6877185ff8b9e34592f834312be2aa53b8d0829a
Brady Todhunter <bradyt@anchore.com>
2019-01-14 13:36:09 -0800

    fully functional argo workflow
    Signed-off-by: Brady Todhunter <bradyt@anchore.com>

--- test/e2e/create_configmap.py
+++ test/e2e/create_configmap.py
@@ -1,7 +1,21 @@
 import yaml
+import sys
+import os
 
-anchore_config = 'load_test/config.yaml'
+# prep input from CLI
+if len(sys.argv) <= 1:
+  print ("USAGE: {} <commit sha> <config dir>".format(sys.argv[0]))
+  sys.exit(1)
+
+commit_sha = str(sys.argv[1])
+config_path = str(sys.argv[2])
+if not os.path.exists(config_path):
+  print ("config dir {} does not exist".format(sys.argv[2]))
+  sys.exit(1)
+
+anchore_config = config_path + '/config.yaml'
 anchore_configmap = 'ship_kustomize/overlays/ship/engine_configmap.yaml'
+anchore_values = config_path + '/values.yaml'
 
 with open(anchore_config, 'r') as config_file:
   config_string = config_file.read()
@@ -14,3 +28,10 @@ configmap['data']['config.yaml'] = config_string
 with open(anchore_configmap, 'w') as configmap_file:
   yaml.safe_dump(configmap, configmap_file, default_flow_style=False)
 
+# with open(anchore_values, 'r') as values_file:
+#   values = yaml.safe_load(values_file)
+
+# values['anchoreGlobal']['image']['tag'] = commit_sha
+
+# with open(anchore_values, 'w') as values_file:
+#   yaml.safe_dump(values, values_file, default_flow_style=False)
